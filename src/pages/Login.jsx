9c7133e31e05968784d907884740e3eecdc8cc71
Straw28 <101749695+Straw28@users.noreply.github.com>
2024-03-18 14:27:46 -0500

    lgin

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import '../styles/header_styles.css';
+
+function Login(){
+    
+    
+    return(
+        <>
+        <div className="login-container-box">
+
+        </div>
+        </>
+    );
+}
+
+export default Login;
