3f7c33866637de8533e31033c46db31a9b8c4b47
annetteptran <158605450+annetteptran@users.noreply.github.com>
2024-03-20 18:34:37 -0500

    :3

--- src/components/top_bar.jsx
+++ src/components/top_bar.jsx
@@ -16,14 +16,14 @@ function TopBar() {
 
 
     return (
-        <div style={{ display:'flex', justifyContent:'flex-end', alignItems:'center', padding:'10px', overflowX:'hidden'}}>
+        <div style={{display:'flex', justifyContent:'flex-end', alignItems:'center', padding:'10px', overflowX:'hidden'}}>
             
-            <Link to='/login'> <button className='button-17' style={{top:'1%', marginRight:'50px', left:'94%'}}>Login</button> </Link> 
-            <button className='button-17' onClick={toggleHours} style={{top:'1%', marginRight:'6%' }}>Hours</button>
+            <Link to='/login'> <button className='button-17' style={{top:'1.5%', marginRight:'20px', left:'93%'}}>Login</button> </Link> 
+            <button className='button-17' onClick={toggleHours} style={{top:'1.5%', marginRight:'7%' }}>Hours</button>
    
             <div className='lib-logo-and-text'>Madea Public Library</div>
             {hoursVisible && (
-                <div style={{position: 'absolute', top: '80px', right: '55px', background: '#f2f2f2', padding: '10px', zIndex:'999', borderRadius: '10px'}}>
+                <div style={{position:'absolute', top:'75px', right:'80px', background:'#f2f2f2', padding:'10px', zIndex:'999', borderRadius:'10px', textAlign:'center'}}>
                     <h3>Library Hours</h3>
                     <p><strong>Mon</strong>&nbsp;&nbsp;&nbsp;&nbsp;12 pm – 9 pm</p>
                     <p><strong>Tue</strong>&nbsp;&nbsp;&nbsp;&nbsp;10 am – 9 pm</p>
