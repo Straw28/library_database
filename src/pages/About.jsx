2ab496255169a66885eded4072dd1cc56ee39ef2
drew r <drewrackovan@gmail.com>
2024-03-20 22:00:16 -0500

    last changes for 3/20 drew

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,9 +9,7 @@ function About(){
       
       <h3>Our Mission</h3>
       <p>Our mission is to promote literacy, lifelong learning, and community engagement by offering a diverse collection of books, resources, and programs for all ages.</p>
-      
-      <h3>Library Hours</h3>
-      <p>We are open Monday through Friday from 9:00 AM to 6:00 PM, and on Saturdays from 10:00 AM to 4:00 PM. We are closed on Sundays and public holidays.</p>
+    
     </div>
     </>
     );
