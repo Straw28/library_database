1325f22de1465bc76829dfbdfb379e86afb2fb07
Straw28 <101749695+Straw28@users.noreply.github.com>
2024-03-22 23:56:05 -0500

    align fixes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -28,59 +28,60 @@ function Profile(){
 
     return (
         <>
-        <div><TopBar/></div>
-            <div className="profile-sidebar-container" style={{marginBottom:'3%'}}>
-                <div className="sidebar-container-box">
-                    <div style={{marginLeft:'2%', color:'#3c4043'}}>
-                        <h1 style={{fontSize:'20px', marginTop:'2%'}}>My Account</h1>
-                        <p style={{fontSize:'20px', marginTop:'5%'}}>{user.name}</p>
-                        <Link to='/' style={{textDecoration:'none', color:'#3c4043'}}>
-                            <div style={{fontSize:'16px', marginTop:'5%', marginLeft:'10%'}}>My Record</div>
-                        </Link>
-                        <Link to='/' style={{textDecoration:'none', color:'#3c4043'}}>
-                            <div style={{fontSize:'16px', marginTop:'5%', marginLeft:'10%'}}>Items Out</div>
-                        </Link>
-                        <Link to='/profile/holds' style={{textDecoration:'none', color:'#3c4043'}}>
-                            <div style={{fontSize:'16px', marginTop:'5%', marginLeft:'10%'}}>Holds</div>
-                        </Link>
-                        <Link to='/' style={{textDecoration:'none', color:'#3c4043'}}>
-                            <div style={{fontSize:'16px', marginTop:'5%', marginLeft:'10%'}}>Fines</div>
-                        </Link>
-                        <Link to='/' style={{textDecoration:'none', color:'#3c4043'}}>
-                            <div style={{fontSize:'16px', marginTop:'5%'}}>Logout</div>
-                        </Link>
+            <div><TopBar/></div>
+            <div className = "profile-container" style = {{display: 'flex', marginLeft:'15%'}}>
+                <div className="profile-sidebar-container" style={{marginBottom:'3%'}}>
+                    <div className="sidebar-container-box" style={{width:'auto', height:'auto', marginTop:'10%'}}>
+                        <div style={{marginLeft:'2%', color:'#3c4043'}}>
+                            <h1 style={{fontSize:'20px', marginTop:'2%'}}>My Account</h1>
+                            <p style={{fontSize:'20px', marginTop:'5%'}}>{user.name}</p>
+                            <Link to='/' style={{textDecoration:'none', color:'#3c4043'}}>
+                                <div style={{fontSize:'16px', marginTop:'5%', marginLeft:'10%'}}>My Record</div>
+                            </Link>
+                            <Link to='/' style={{textDecoration:'none', color:'#3c4043'}}>
+                                <div style={{fontSize:'16px', marginTop:'5%', marginLeft:'10%'}}>Items Out</div>
+                            </Link>
+                            <Link to='/profile/holds' style={{textDecoration:'none', color:'#3c4043'}}>
+                                <div style={{fontSize:'16px', marginTop:'5%', marginLeft:'10%'}}>Holds</div>
+                            </Link>
+                            <Link to='/' style={{textDecoration:'none', color:'#3c4043'}}>
+                                <div style={{fontSize:'16px', marginTop:'5%', marginLeft:'10%'}}>Fines</div>
+                            </Link>
+                            <Link to='/' style={{textDecoration:'none', color:'#3c4043'}}>
+                                <div style={{fontSize:'16px', marginTop:'5%'}}>Logout</div>
+                            </Link>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <div className="profile-container-box">
-                    <div style={{marginLeft:'20px', fontSize:'20px', color:'#3c4043'}}>
-                        <h1 style={{marginTop:'5%'}}>User Profile</h1>
-                        <p style={{marginTop:'5%'}}><strong>Name:</strong> {user.name}</p>
-                        <p style={{marginTop:'2%'}}><strong>Barcode:</strong> {user.barcode}</p>
-                        <p style={{marginTop:'2%'}}><strong>Username:</strong> {user.username}</p>
-                        <p style={{marginTop:'2%'}}><strong>Address:</strong> {user.address}</p>
-                        <p style={{marginTop:'2%'}}><strong>Date Registered:</strong> {user.dateRegistered}</p>
-                        <p style={{marginTop:'2%'}}><strong>Birthdate:</strong> {user.birthdate}</p>
+                    <div>
+                        <div className="profile-container-box">
+                        <div style={{marginLeft:'20px', fontSize:'20px', color:'#3c4043'}}>
+                            <h1 style={{marginTop:'5%'}}>User Profile</h1>
+                            <p style={{marginTop:'5%'}}><strong>Name:</strong> {user.name}</p>
+                            <p style={{marginTop:'2%'}}><strong>Barcode:</strong> {user.barcode}</p>
+                            <p style={{marginTop:'2%'}}><strong>Username:</strong> {user.username}</p>
+                            <p style={{marginTop:'2%'}}><strong>Address:</strong> {user.address}</p>
+                            <p style={{marginTop:'2%'}}><strong>Date Registered:</strong> {user.dateRegistered}</p>
+                            <p style={{marginTop:'2%'}}><strong>Birthdate:</strong> {user.birthdate}</p>
+                        </div>
+                        </div>
+                        <div style={{marginTop:'3%', marginLeft:'5%'}}>
+                            <button onClick={toggleMessagesDropdown}>Messages</button>
+                            {showMessagesDropdown && (
+                                <div className="dropdown-info-container" style={{marginLeft:'0%'}}>
+                                    <p style={{marginTop:'1%', marginBottom:'1%', marginLeft:'1%'}}>message :p</p>
+                                </div>
+                            )}
+                        </div>
+                        <div style={{marginTop:'3%', marginLeft:'5%'}}>
+                            <button onClick={toggleUserDropdown}>Change User/Password</button>
+                            {showUserDropdown && (
+                                <div className="dropdown-info-container">
+                                    {/* Dropdown content for Change User/Password */}
+                                </div>
+                            )}
+                        </div>
                     </div>
                     </div>
-                    <div style={{marginTop:'3%', marginLeft:'5%'}}>
-                        <button onClick={toggleMessagesDropdown}>Messages</button>
-                        {showMessagesDropdown && (
-                            <div className="dropdown-info-container" style={{marginRight:'10%'}}>
-                                <p style={{marginTop:'1%', marginBottom:'1%', marginLeft:'1%'}}>message :p</p>
-                            </div>
-                        )}
-                    </div>
-                    <div style={{marginTop:'3%', marginLeft:'5%'}}>
-                        <button onClick={toggleUserDropdown}>Change User/Password</button>
-                        {showUserDropdown && (
-                            <div className="dropdown-info-container">
-                                {/* Dropdown content for Change User/Password */}
-                            </div>
-                        )}
-                    </div>
-                </div>
-                
 
             </div>
 
