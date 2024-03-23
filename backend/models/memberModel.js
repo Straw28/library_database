1d12808167e2f452f7cbea7be87fe06bf94c2e5a
Straw28 <101749695+Straw28@users.noreply.github.com>
2024-03-23 16:46:29 -0500

    hashing password

--- backend/models/memberModel.js
+++ backend/models/memberModel.js
@@ -1,4 +1,5 @@
 import pool from './database.js';
+import bcrypt from 'bcrypt';
 
 class MemberModel{
     /**
@@ -33,13 +34,14 @@ class MemberModel{
 
            const { street, unit, city, state, postalCode } = address;
            const fullAddress = `${street}, ${unit}, ${city}, ${state}, ${postalCode}`;
-            console.log(first_name, last_name);
+           const hashedPassword = await bcrypt.hash(password, 10); // 10 is the number of salt rounds
+            
             // executing the query
             await pool.query(`
                 INSERT INTO members 
                     (library_card_number, member_status, member_type, first_name, last_name, email_address, address, date_of_birth, item_borrowing_history, device_borrowing_history, registration_date, expiration_date, requests, fine_id, password, phone_number) 
                 VALUES 
-                (UUID(), 'active', 'regular', '${first_name}', '${last_name}', '${email}', '${fullAddress}', '${date_of_birth}', NULL, NULL, CURDATE(), NULL, NULL, NULL, '${password}', '${phone_number}');
+                (UUID(), 'active', 'regular', '${first_name}', '${last_name}', '${email}', '${fullAddress}', '${date_of_birth}', NULL, NULL, CURDATE(), NULL, NULL, NULL, '${hashedPassword}', '${phone_number}');
             `);
 
 
